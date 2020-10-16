92df27b38197d38f2380fe2575504ef2f94ca619
PierGiorgioMingoia <piergiorgio.mingoia@gmail.com>
2020-10-16 18:56:21 +0200

    SVM test with sklearn and pytorch

---
+++ my_ex/SVN/svm_sklearn.py
@@ -0,0 +1,56 @@
+from sklearn import svm
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import svm, datasets
+
+# data to play with
+iris = datasets.load_iris()
+
+X = iris.data[:, :2]  # only two first features
+
+y = iris.target
+
+h = 0.2  # step size
+
+print(len(X))
+print(len(y))
+
+# instance of SVM and fit out data, plot support vectors
+
+C = 100.0  # SVM regularization parameter
+
+svc = svm.SVC(kernel='linear', C=C).fit(X, y)
+
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(X, y)
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(X, y)
+lin_svc = svm.LinearSVC(C=C, dual=False).fit(X, y)
+
+# create the mesh for the plot
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+titles = ['SVC with linear kernel', 'Linear SVC (linear kernel)', 'SVC with RBF kernel',
+          'SVC with polynomial (degree 3) kernel']
+
+for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
+    plt.subplot(2, 2, i + 1)
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+
+    # Training point
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
+    plt.xlabel('Sepal length')
+    plt.ylabel('Sepal width')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.xticks(())
+    plt.yticks(())
+    plt.title(titles[1])
+
+plt.show()
